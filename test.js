d3b302f12c1cd7abcdb7fe8509ad2c1e676c3183
Joe Wright <joe@joejag.com>
2014-11-30 14:55:26 +0000

    Joe - using mocha

---
+++ test.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect
+var FontScaler = require('./index')
+
+describe('Font Scalar', function() {
+
+    it('constrained by width', function() {
+        // height is larger than the width constraint
+
+        // longest word is size 4, plus padding of 3 so longest line is 7 characters
+        // width of single letter is 6, so longest line is 42
+        // equation is (width / longestLineSize) * fontSizeForSingleLetter
+        // (100 / 42) * 10 = 23
+        var width = 100
+
+        expect(new FontScaler(['word'], 50, width).ideal()).to.equal(23)
+    })
+
+    it('constrained by height', function() {
+        // width is larger than the height constraint
+
+        // work out maximum number of number of lines (set by the width), in this case 1
+        // equation is (height / largestNumberOfLines) / heightOfSingleLetter * fontSizeForSingleLetter
+        // (20 / 1) / 13 * 10 = 15
+
+        expect(new FontScaler(['word'], 20, 100).ideal()).to.equal(15)
+    })
+
+    it('constrained by height over multiple lines', function() {
+        // width is larger than the height constraint
+
+        // work out maximum number of number of lines (set by the width), in this case 1
+        // equation is (height / largestNumberOfLines) / heightOfSingleLetter * fontSizeForSingleLetter
+        // (20 / 3) / 13 * 10 = 5
+
+        expect(new FontScaler(['word word word'], 20, 100).ideal()).to.equal(5)
+    })
+
+})
